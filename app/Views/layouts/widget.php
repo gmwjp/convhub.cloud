c38ec9b37ae8446b683e408e6d3001c4c374e68d
原　綾志 <rhara@g-m-w.jp>
2024-02-28 14:04:12 +0900

    調査

--- app/Views/layouts/widget.php
+++ app/Views/layouts/widget.php
@@ -1,10 +1,55 @@
 <!doctype html>
 <html lang="en">
-	<head>
-	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src 'unsafe-inline'; img-src data:; connect-src 'self'">
-	</head>
-	<body>
-	test
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Feedback</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <style>
+    @media (prefers-color-scheme: light) {
+      body {
+        background-color: #ffffff; /* 明るい背景色 */
+        color: #000000; /* 暗いテキスト色 */
+      }
+    }
+    @media (prefers-color-scheme: dark) {
+      body {
+        background-color: #343a40; /* Bootstrapのダークテーマの背景色に合わせた */
+        color: #ffffff; /* 明るいテキスト色 */
+      }
+    }
+    </style>
+	<script <?=csp_script_nonce_test()?>>
+		var csrf_token_name = '<?=csrf_token()?>';
+		var csrf_token_value = '<?=csrf_hash()?>';
+		function postData(url,data,done_func=null,fail_func=null){
+			$.ajax({
+				url: '/widgets/get_token',
+				method: 'GET',
+				dataType: 'json',
+				success: function(data) {
+					csrf_token_value = data.value;
+					$('input[name="' + csrf_token_name + '"]').val(csrf_token_value); 
+				},
+				error: function() {
+					console.error('CSRF取得エラー');
+				}
+			}).done(function(){
+				data[csrf_token_name] = csrf_token_value;
+				$.post(url,data).done(function(data, textStatus, jqXHR) {
+					csrf_token_value = jqXHR.getResponseHeader('X-CSRF-TOKEN');
+					$('input[name="' + csrf_token_name + '"]').val(csrf_token_value);
+					if(done_func) done_func(data);
+				}).fail(function(data){
+					if(fail_func) fail_func(data);
+				});
+			});
+		}
+	</script>
+  </head>
+  <body>
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+	<?=$content?>
   </body>
 </html>
