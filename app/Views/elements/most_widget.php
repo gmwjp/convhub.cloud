d1fca51d7cce9d2a71b8fbe56ee9b2621fa4c6d7
原　綾志 <rhara@g-m-w.jp>
2024-03-12 18:22:44 +0900

    調整

--- app/Views/elements/most_widget.php
+++ app/Views/elements/most_widget.php
@@ -3,4 +3,6 @@
     <div class="mt-2"><a  target="_blank" href="<?=esc($widget->notion_url)?>" style="font-weight:normal;"><?=esc($widget->name)?><i class="ml-1 fal fa-external-link"></i></a></div>
 <?}?>
 <hr>
-<a href="<?=esc($form->notion_url)?>" target="_blank">ヘルプセンターへ<i class="ml-1 fal fa-external-link"></i></a>
+<div class="text-center">
+    <a href="<?=esc($form->notion_url)?>" target="_blank" class="btn btn-light">ヘルプセンターへ<i class="ml-1 fal fa-external-link"></i></a>
+</div>
