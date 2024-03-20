efddb68ccd554ce7c425475339c2a1bec2947e6b
原　綾志 <rhara@g-m-w.jp>
2024-03-21 02:31:17 +0900

    調整

--- app/Views/forms/widgets.php
+++ app/Views/forms/widgets.php
@@ -23,7 +23,7 @@
                         <a href="/widgets/detail/<?=esc($widget->id)?>" data-toggle="tooltip" data-placement="top" title="<?=esc($widget->name)?>"><?=cutWord(esc($widget->name),20)?></a>
                         <sapn class="ml-2"><button type="button" data-toggle="tooltip" data-placement="top" title="ウィジェットURLをコピー" class="btn btn-light btn-sm copy_button" data-widget-url="<?=$_SERVER["SITE_URL"]?>/widgets/show/<?=esc($widget->code)?>"><span class="fal fa-copy"></span></button></span>
                         <?if($widget->notion_url !=""){?>
-                            <sapn class="ml-1"><a href="<?=esc($widget->notion_url)?>" target="_blank" data-toggle="tooltip" data-placement="top" title="Notion記事を開く" class="btn btn-light btn-sm"><span class="fal fa-external-link"></span></a></span>
+                            <sapn class="ml-1"><a href="<?=esc($widget->notion_url)?>" target="_blank" data-toggle="tooltip" data-placement="top" title="記事を管理" class="btn btn-light btn-sm"><span class="fal fa-external-link"></span></a></span>
                         <?}?>
                         <input type="hidden" id="widget_url_<?=esc($widget->id)?>" class="form-control" readonly="true" value="<?=$_SERVER["SITE_URL"]?>/widgets/show/<?=esc($widget->code)?>">
                     </div>
